26f0c1a403cd578281178514cae4b626effcdf1d
dyaaahmed <49793432+dyaaahmed@users.noreply.github.com>
2020-04-18 17:06:55 +0200

    Update dumpmogodb.sh

--- dumpmogodb.sh
+++ dumpmogodb.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 #This script dump mongodb containers
 
-# Defining vars 
-CONTAINER_NAME="$(docker ps | grep -i mongo |grep -iv sec | awk {'print $1'})"
+# list of mongodb containers ids
+CONTAINER_NAME="$(docker ps | grep -i mongo |grep -iv sec | awk '{print $(NF)}')"
+
+#Looping on mutliple containers id
+for i in $CONTAINER_NAME; do
+
+#defining variables
 TIME_STAMP="$(date +'%Y%m%d')"
-BACKUP_DIR=/var/mongobackup
-USER_NAME="$(docker inspect $CONTAINER_NAME | grep -i MONGODB_USERNAME | cut -d "=" -f2 | cut -d "\"" -f1 )"
-PASSWORD=$(docker inspect $CONTAINER_NAME | grep -i MONGODB_PASSWORD | cut -d "=" -f2 | cut -d "\"" -f1)
-DB=$(docker inspect $CONTAINER_NAME | grep -i MONGODB_DATABASE | cut -d "=" -f2 | cut -d "\"" -f1)
+BACKUP_DIR=/var/mongobackup/$i
+USER_NAME="$(docker inspect $i | grep -i MONGODB_USERNAME | cut -d "=" -f2 | cut -d "\"" -f1 )"
+PASSWORD=$(docker inspect $i | grep -i MONGODB_PASSWORD | cut -d "=" -f2 | cut -d "\"" -f1)
+DB=$(docker inspect $i | grep -i MONGODB_DATABASE | cut -d "=" -f2 | cut -d "\"" -f1)
 CONTAINER_DIR=/bitnami
 
 # Creating Backup Directory
@@ -18,21 +23,24 @@ fi
 
 #Backup DataBase
 echo "Backing up DataBase"
-docker exec -t $CONTAINER_NAME mkdir -p $CONTAINER_DIR/$TIME_STAMP 
-docker exec -t $CONTAINER_NAME mongodump --host 127.0.0.1 --port=27017 --username=$USER_NAME --password=$PASSWORD --authenticationDatabase=$DB --db=$DB --out=$CONTAINER_DIR/$TIME_STAMP
+docker exec -t $i mkdir -p $CONTAINER_DIR/$TIME_STAMP 
+docker exec -t $i mongodump --host 127.0.0.1 --port=27017 --username=$USER_NAME --password=$PASSWORD --authenticationDatabase=$DB --db=$DB --out=$CONTAINER_DIR/$TIME_STAMP
 
-# Coping files from container 
-docker cp $CONTAINER_NAME:$CONTAINER_DIR/$TIME_STAMP ./
-docker exec -t $CONTAINER_NAME rm -rf $CONTAINER_DIR/$TIME_STAMP
+# Coping files from container and removing backup from containers 
+docker cp $i:$CONTAINER_DIR/$TIME_STAMP ./
+docker exec -t $i rm -rf $CONTAINER_DIR/$TIME_STAMP
+
 #Archiving and removing backup dir
 tar cvf $TIME_STAMP.tar $TIME_STAMP
-cp $TIME_STAMP.tar $BACKUP_DIR 
+cp $TIME_STAMP.tar $BACKUP_DIR/$i-$TIME_STAMP.tar
 rm -rf $TIME_STAMP*
 
-
-#Check If Backup Created Successfully
+#Check If Backup Created Successfully in each container
 if [ $? -eq 0 ]; then 
-	echo "Backup Done successfully"
+	echo "$i Backup Done successfully"
 else
-	echo "Something Went Wrong"
+	echo "$i Something Went Wrong"
 fi
+
+done
+
